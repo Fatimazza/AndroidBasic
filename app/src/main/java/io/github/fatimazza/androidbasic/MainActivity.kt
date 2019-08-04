18659f75b622d61271fc92fb8d89cd36efb5b79a
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-04 21:25:06 +0700

    Activity - Fix layout id, Init views in Activity

--- app/src/main/java/io/github/fatimazza/androidbasic/MainActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/MainActivity.kt
@@ -1,12 +1,27 @@
 package io.github.fatimazza.androidbasic
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var edtLength: EditText
+    private lateinit var edtWidth: EditText
+    private lateinit var edtHeight: EditText
+    private lateinit var btnCalculate: Button
+    private lateinit var tvResult: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        edtLength = findViewById(R.id.edt_length)
+        edtWidth = findViewById(R.id.edt_width)
+        edtHeight = findViewById(R.id.edt_height)
+        btnCalculate = findViewById(R.id.btn_calculate)
+        tvResult = findViewById(R.id.tv_result)
     }
 }
