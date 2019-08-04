9e769d3d867faade89233c0e1516524edac78cf3
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-04 22:10:37 +0700

    Intent - Add Button layout, logic, and onClick Listener

--- app/src/main/java/io/github/fatimazza/androidbasic/IntentActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/IntentActivity.kt
@@ -1,12 +1,24 @@
 package io.github.fatimazza.androidbasic
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
 
-class IntentActivity : AppCompatActivity() {
+class IntentActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent)
+
+        val btnMove: Button = findViewById(R.id.btn_move_activity)
+        btnMove.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.btn_move_activity -> {
+            }
+        }
     }
 }
