4dbe2b536dfd168929190613a250e83600f322cc
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-05 15:24:06 +0700

    Explicit Intent - Add new Activity to receive Intent Data

--- app/src/main/java/io/github/fatimazza/androidbasic/IntentMoveDataActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/IntentMoveDataActivity.kt
@@ -1,12 +1,26 @@
 package io.github.fatimazza.androidbasic
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.TextView
 
 class IntentMoveDataActivity : AppCompatActivity() {
+
+    companion object {
+        const val EXTRA_AGE = "extra_age"
+        const val EXTRA_NAME = "extra_name"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent_move_data)
+
+        val tvDataReceived: TextView = findViewById(R.id.tv_data_received)
+
+        val name = intent.getStringExtra(EXTRA_NAME)
+        val age = intent.getIntExtra(EXTRA_AGE, 0)
+
+        val text = "Nama : $name, Age : $age"
+        tvDataReceived.text = text
     }
 }
