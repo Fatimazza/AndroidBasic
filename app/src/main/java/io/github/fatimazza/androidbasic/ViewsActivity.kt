6406ade261a536a5495cb4f6d5014154bfd3fd0e
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-06 11:33:14 +0700

    Views ViewGroup - Add Title in ActionBar

--- app/src/main/java/io/github/fatimazza/androidbasic/ViewsActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/ViewsActivity.kt
@@ -1,12 +1,18 @@
 package io.github.fatimazza.androidbasic
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.ActionBar
+import android.support.v7.app.AppCompatActivity
 
 class ViewsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_views)
+
+        if (supportActionBar != null) {
+            (supportActionBar as ActionBar).title = "Google Pixel"
+        }
+
     }
 }
