caaae51ffc65ae6403f8e2235dd44e3f71622fca
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-07 11:50:28 +0700

    RecyclerView List - Add Initial Adapter

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/adapter/ListHeroAdapter.kt
@@ -0,0 +1,24 @@
+package io.github.fatimazza.androidbasic.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class ListHeroAdapter : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ListViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: ListViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
