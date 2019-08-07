335e6e0cfee573664cd6b96bf4dc3233627a8045
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-07 13:53:02 +0700

    RecyclerView List - Implement functions in Adapter

--- app/src/main/java/io/github/fatimazza/androidbasic/adapter/ListHeroAdapter.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/adapter/ListHeroAdapter.kt
@@ -1,24 +1,43 @@
 package io.github.fatimazza.androidbasic.adapter
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import io.github.fatimazza.androidbasic.R
+import io.github.fatimazza.androidbasic.model.Hero
 
-class ListHeroAdapter : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
+class ListHeroAdapter(val listHero: ArrayList<Hero>) : RecyclerView.Adapter<ListHeroAdapter.ListViewHolder>() {
 
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): ListViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view: View = LayoutInflater.from(viewGroup.context)
+            .inflate(R.layout.item_row_hero, viewGroup, false)
+        return ListViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return listHero.size
     }
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val (name, origin, photo) = listHero[position]
+
+        Glide.with(holder.itemView.context)
+            .load(photo)
+            .apply(RequestOptions().override(55, 55))
+            .into(holder.imgPhoto)
+
+        holder.tvName.text = name
+        holder.tvOrigin.text = origin
     }
 
     inner class ListViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        var tvOrigin: TextView = itemView.findViewById(R.id.tv_item_origin)
+        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
     }
 }
