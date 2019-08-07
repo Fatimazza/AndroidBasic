f42f6772cd8624400a29d5c8dc5ab88e0c089847
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-07 16:29:17 +0700

    RecyclerView Grid - Add Initial Adapter

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/adapter/GridHeroAdapter.kt
@@ -0,0 +1,24 @@
+package io.github.fatimazza.androidbasic.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class GridHeroAdapter : RecyclerView.Adapter<GridHeroAdapter.GridViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): GridHeroAdapter.GridViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: GridViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class GridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
