1d27b69287e05561614a7d3146121b8bb2f5843b
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-07 13:59:51 +0700

    RecyclerView List - Show List in Activity

--- app/src/main/java/io/github/fatimazza/androidbasic/RecyclerViewActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/RecyclerViewActivity.kt
@@ -1,12 +1,32 @@
 package io.github.fatimazza.androidbasic
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
+import android.support.v7.widget.RecyclerView
+import io.github.fatimazza.androidbasic.adapter.ListHeroAdapter
+import io.github.fatimazza.androidbasic.model.Hero
+import io.github.fatimazza.androidbasic.model.HeroesData
 
 class RecyclerViewActivity : AppCompatActivity() {
+
+    private lateinit var rvHeroes: RecyclerView
+    private var list: ArrayList<Hero> = arrayListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_recycler_view)
+
+        rvHeroes = findViewById(R.id.rv_hero)
+        rvHeroes.setHasFixedSize(true)
+
+        list.addAll(HeroesData.listData)
+        showRecyclerList()
+    }
+
+    private fun showRecyclerList() {
+        rvHeroes.layoutManager = LinearLayoutManager(this)
+        val listHeroAdapter = ListHeroAdapter(list)
+        rvHeroes.adapter = listHeroAdapter
     }
 }
