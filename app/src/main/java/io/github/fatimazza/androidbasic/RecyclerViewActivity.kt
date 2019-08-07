956884eea26ec52e690937f2ad4a42768736fe93
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-07 15:13:59 +0700

    Item Menu - Show Menu Item on Activity

--- app/src/main/java/io/github/fatimazza/androidbasic/RecyclerViewActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/RecyclerViewActivity.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
+import android.view.Menu
+import android.view.MenuItem
 import io.github.fatimazza.androidbasic.adapter.ListHeroAdapter
 import io.github.fatimazza.androidbasic.model.Hero
 import io.github.fatimazza.androidbasic.model.HeroesData
@@ -28,5 +30,29 @@ class RecyclerViewActivity : AppCompatActivity() {
         rvHeroes.layoutManager = LinearLayoutManager(this)
         val listHeroAdapter = ListHeroAdapter(list)
         rvHeroes.adapter = listHeroAdapter
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_recycler_view, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        setMode(item.itemId)
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setMode(selectedMode: Int) {
+        when (selectedMode) {
+            R.id.action_list -> {
+
+            }
+            R.id.action_grid -> {
+
+            }
+            R.id.action_cardview -> {
+
+            }
+        }
     }
 }
