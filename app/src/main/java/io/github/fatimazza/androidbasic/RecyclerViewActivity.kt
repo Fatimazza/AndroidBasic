07aee7a43082d61eafa7efbc270e10bd82f611fe
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-13 21:34:52 +0700

    RecyclerView Grid - Show Grid in Activity

--- app/src/main/java/io/github/fatimazza/androidbasic/RecyclerViewActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/RecyclerViewActivity.kt
@@ -2,10 +2,12 @@ package io.github.fatimazza.androidbasic
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
 import android.view.Menu
 import android.view.MenuItem
+import io.github.fatimazza.androidbasic.adapter.GridHeroAdapter
 import io.github.fatimazza.androidbasic.adapter.ListHeroAdapter
 import io.github.fatimazza.androidbasic.model.Hero
 import io.github.fatimazza.androidbasic.model.HeroesData
@@ -32,6 +34,12 @@ class RecyclerViewActivity : AppCompatActivity() {
         rvHeroes.adapter = listHeroAdapter
     }
 
+    private fun showRecyclerGrid() {
+        rvHeroes.layoutManager = GridLayoutManager(this, 2)
+        val gridHeroAdapter = GridHeroAdapter(list)
+        rvHeroes.adapter = gridHeroAdapter
+    }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         menuInflater.inflate(R.menu.menu_recycler_view, menu)
         return super.onCreateOptionsMenu(menu)
@@ -45,10 +53,10 @@ class RecyclerViewActivity : AppCompatActivity() {
     private fun setMode(selectedMode: Int) {
         when (selectedMode) {
             R.id.action_list -> {
-
+                showRecyclerList()
             }
             R.id.action_grid -> {
-
+                showRecyclerGrid()
             }
             R.id.action_cardview -> {
 
