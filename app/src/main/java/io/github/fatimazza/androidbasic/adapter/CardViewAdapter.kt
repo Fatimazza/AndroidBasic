eefa429ab8606794d0dac269a777cf1da9972553
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-13 22:20:47 +0700

    RecyclerView CardView - Implements functions in Adapter

--- app/src/main/java/io/github/fatimazza/androidbasic/adapter/CardViewAdapter.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/adapter/CardViewAdapter.kt
@@ -1,25 +1,48 @@
 package io.github.fatimazza.androidbasic.adapter
 
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import io.github.fatimazza.androidbasic.R
+import io.github.fatimazza.androidbasic.model.Hero
 
-class CardViewAdapter : RecyclerView.Adapter<CardViewAdapter.CardviewViewHolder>() {
+class CardViewAdapter(private val listHeroes: ArrayList<Hero>) :
+    RecyclerView.Adapter<CardViewAdapter.CardviewViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardviewViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val view: View = LayoutInflater
+            .from(parent.context).inflate(R.layout.item_cardview_hero, parent, false)
+        return CardviewViewHolder(view)
     }
 
     override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        return listHeroes.size
     }
 
     override fun onBindViewHolder(holder: CardviewViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val (name, origin, photo) = listHeroes[position]
+
+        Glide.with(holder.itemView.context)
+            .load(photo)
+            .apply(RequestOptions().override(350, 550))
+            .into(holder.imgPhoto)
+
+        holder.tvName.text = name
+        holder.tvOrigin.text = origin
     }
 
     inner class CardviewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+        var tvName: TextView = itemView.findViewById(R.id.tv_item_name)
+        var tvOrigin: TextView = itemView.findViewById(R.id.tv_item_origin)
+        var btnFavorite: Button = itemView.findViewById(R.id.btn_favorite)
+        var btnShare: Button = itemView.findViewById(R.id.btn_share)
     }
 
 }
