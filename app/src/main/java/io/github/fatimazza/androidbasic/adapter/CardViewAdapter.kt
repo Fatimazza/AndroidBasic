3f7afe4ce1bcd00921d29ff08d0fa9287bcafe2e
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-13 21:59:34 +0700

    RecyclerView CardView - Add Initial Adapter

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/adapter/CardViewAdapter.kt
@@ -0,0 +1,25 @@
+package io.github.fatimazza.androidbasic.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import android.view.ViewGroup
+
+class CardViewAdapter : RecyclerView.Adapter<CardViewAdapter.CardviewViewHolder>() {
+
+    override fun onCreateViewHolder(p0: ViewGroup, p1: Int): CardviewViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(p0: CardviewViewHolder, p1: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class CardviewViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+}
