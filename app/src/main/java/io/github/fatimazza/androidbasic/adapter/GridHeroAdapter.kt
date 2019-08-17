25741fe114f3aec875545bfcb98f37e85d2e61ea
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-08-17 14:02:32 +0700

    Grid ItemOnClickListener -  Implement onClick Callback in Grid Adapter

--- app/src/main/java/io/github/fatimazza/androidbasic/adapter/GridHeroAdapter.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/adapter/GridHeroAdapter.kt
@@ -12,6 +12,12 @@ import io.github.fatimazza.androidbasic.model.Hero
 
 class GridHeroAdapter(val listHeroes: ArrayList<Hero>) : RecyclerView.Adapter<GridHeroAdapter.GridViewHolder>() {
 
+    private lateinit var onItemClickCallback: GridHeroAdapter.OnItemClickCallback
+
+    fun setOnItemClickCallback(onItemClickCallback: OnItemClickCallback) {
+        this.onItemClickCallback = onItemClickCallback
+    }
+
     override fun onCreateViewHolder(viewGroup: ViewGroup, i: Int): GridHeroAdapter.GridViewHolder {
         val view: View = LayoutInflater.from(viewGroup.context)
             .inflate(R.layout.item_grid_hero, viewGroup, false)
@@ -27,9 +33,15 @@ class GridHeroAdapter(val listHeroes: ArrayList<Hero>) : RecyclerView.Adapter<Gr
             .load(listHeroes[position].photo)
             .apply(RequestOptions().override(350, 550))
             .into(holder.imgPhoto)
+
+        holder.itemView.setOnClickListener { onItemClickCallback.onItemClick(listHeroes[holder.adapterPosition]) }
     }
 
     inner class GridViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         var imgPhoto: ImageView = itemView.findViewById(R.id.img_item_photo)
+    }
+
+    interface OnItemClickCallback {
+        fun onItemClick(data: Hero)
     }
 }
