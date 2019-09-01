45d0abed87d87481dbb661e6229bfa420e5a627b
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-09-01 20:13:32 +0700

    Fragment - Show Home Fragment in Activity

--- app/src/main/java/io/github/fatimazza/androidbasic/FragmentActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/FragmentActivity.kt
@@ -1,12 +1,25 @@
 package io.github.fatimazza.androidbasic
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import io.github.fatimazza.androidbasic.fragment.HomeFragment
+
 
 class FragmentActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_fragment)
+
+        initFragment()
+    }
+
+    private fun initFragment() {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+
+        val fragment = HomeFragment()
+        fragmentTransaction.add(R.id.frame_container, fragment)
+        fragmentTransaction.commit()
     }
 }
