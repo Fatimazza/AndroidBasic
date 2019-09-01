d1e13e78fa02e114188f7a45b988a09f9ca00405
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-09-01 19:33:42 +0700

    Fragment - Add Layout and onClick Listener in Home Fragment

--- app/src/main/java/io/github/fatimazza/androidbasic/fragment/HomeFragment.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/fragment/HomeFragment.kt
@@ -6,11 +6,16 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 
 import io.github.fatimazza.androidbasic.R
+import kotlinx.android.synthetic.main.fragment_home.*
 
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(), View.OnClickListener {
+
+    private val btnOther: Button
+        get() = btn_other_fragment
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -19,4 +24,12 @@ class HomeFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnOther.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        
+    }
 }
