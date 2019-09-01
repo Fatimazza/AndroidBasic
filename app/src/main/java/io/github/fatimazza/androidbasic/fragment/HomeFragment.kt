f7e5a25ac0663c3b289850bd8d57247c5d91319f
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-09-01 19:30:35 +0700

    Fragment - Add Home Fragment

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/fragment/HomeFragment.kt
@@ -0,0 +1,22 @@
+package io.github.fatimazza.androidbasic.fragment
+
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+import io.github.fatimazza.androidbasic.R
+
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
