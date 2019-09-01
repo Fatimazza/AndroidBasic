63623733a42cabebf430552acb54df18854304b6
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-09-01 17:49:21 +0700

    Explicit Intent Parcelable - Add other Activity to move using Intent with Object

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/IntentMoveObjectActivity.kt
@@ -0,0 +1,12 @@
+package io.github.fatimazza.androidbasic
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class IntentMoveObjectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_intent_move_object)
+    }
+}
