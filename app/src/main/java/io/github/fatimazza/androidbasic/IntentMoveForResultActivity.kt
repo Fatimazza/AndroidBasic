ff22c7b38730b9c9971bae659e872f22ec1d2cf3
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-09-01 18:32:41 +0700

    Intent for Result - Add RadioGroup in Layout, Setup Click Listener

--- app/src/main/java/io/github/fatimazza/androidbasic/IntentMoveForResultActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/IntentMoveForResultActivity.kt
@@ -1,12 +1,38 @@
 package io.github.fatimazza.androidbasic
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.Button
+import android.widget.RadioGroup
+import kotlinx.android.synthetic.main.activity_intent_move_result.*
 
-class IntentMoveForResultActivity : AppCompatActivity() {
+class IntentMoveForResultActivity : AppCompatActivity(), View.OnClickListener {
+
+    private val rgNumber: RadioGroup
+        get() = rg_number
+
+    private val btnChoose: Button
+        get() = btn_choose
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_intent_move_result)
+
+        rgNumber.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btn_choose) {
+            if (rgNumber.checkedRadioButtonId != 0) {
+                var value = 0
+                when (rgNumber.checkedRadioButtonId) {
+                    R.id.rb_50 -> value = 50
+                    R.id.rb_100 -> value = 100
+                    R.id.rb_150 -> value = 150
+                    R.id.rb_200 -> value = 200
+                }
+            }
+        }
     }
 }
