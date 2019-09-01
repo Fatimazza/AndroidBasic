42ca9930518b2db81a1f911aae4c938428c98be3
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-09-01 22:02:47 +0700

    Dialog Fragment - Add Layout and onClick Listener in Option Dialog Fragment

--- app/src/main/java/io/github/fatimazza/androidbasic/fragment/OptionDialogFragment.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/fragment/OptionDialogFragment.kt
@@ -6,17 +6,57 @@ import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.RadioButton
+import android.widget.RadioGroup
 
 import io.github.fatimazza.androidbasic.R
+import kotlinx.android.synthetic.main.fragment_option_dialog.*
 
 
-class OptionDialogFragment : DialogFragment() {
+class OptionDialogFragment : DialogFragment(), View.OnClickListener {
+
+    private val rgOptions: RadioGroup
+        get() = rg_options
+
+    private val rbBlue: RadioButton
+        get() = rb_blue
+
+    private val rbRed: RadioButton
+        get() = rb_red
+
+    private val rbPurple: RadioButton
+        get() = rb_purple
+
+    private val rbGreen: RadioButton
+        get() = rb_green
+
+    private val btnChoose: Button
+        get() = btn_dialog_choose
+
+    private val btnClose: Button
+        get() = btn_dialog_close
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         return inflater.inflate(R.layout.fragment_option_dialog, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btnChoose.setOnClickListener(this)
+        btnChoose.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.btn_dialog_choose -> {
+            }
+            R.id.btn_dialog_close -> {
+            }
+        }
     }
 
 }
