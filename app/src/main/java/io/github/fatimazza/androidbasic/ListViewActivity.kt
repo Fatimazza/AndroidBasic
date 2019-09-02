6ea7de4cfc01d19aa531012d2c8806abd330a4b7
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-09-02 17:29:59 +0700

    ListView List - Add new Activity for ListView demo, Make it first launch activity

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/ListViewActivity.kt
@@ -0,0 +1,12 @@
+package io.github.fatimazza.androidbasic
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ListViewActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_view)
+    }
+}
