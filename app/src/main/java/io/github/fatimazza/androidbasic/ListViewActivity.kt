250a225e03cf8ced9f0f3d703743115aed1b34db
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-09-02 17:38:49 +0700

    ListView List - Fetch Data using Array Adapter

--- app/src/main/java/io/github/fatimazza/androidbasic/ListViewActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/ListViewActivity.kt
@@ -2,11 +2,24 @@ package io.github.fatimazza.androidbasic
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import kotlinx.android.synthetic.main.activity_list_view.*
 
 class ListViewActivity : AppCompatActivity() {
+
+    private val listView: ListView
+        get() = lv_listview
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list_view)
+
+        val dataName = arrayOf(
+            "Cut Nyak Dien", "Ki Hajar Dewantara", "Moh Yamin", "Patimura", "R A Kartini", "Sukarno"
+        )
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, dataName)
+        listView.adapter = adapter
     }
 }
