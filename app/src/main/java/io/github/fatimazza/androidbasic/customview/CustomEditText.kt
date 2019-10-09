136fcf86b802b3c5e63ed20c3a875c200e7e31f2
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-09 18:39:25 +0700

    Custom View - Implement Init & onDraw Function in Custom EditText class

--- app/src/main/java/io/github/fatimazza/androidbasic/customview/CustomEditText.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/customview/CustomEditText.kt
@@ -2,17 +2,23 @@ package io.github.fatimazza.androidbasic.customview
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.drawable.Drawable
+import android.support.v4.content.res.ResourcesCompat
 import android.support.v7.widget.AppCompatEditText
 import android.util.AttributeSet
+import android.view.View
+import io.github.fatimazza.androidbasic.R
 
 class CustomEditText : AppCompatEditText {
 
-    constructor(context: Context) : super(context) {
+    internal lateinit var imgClearButton: Drawable
 
+    constructor(context: Context) : super(context) {
+        init()
     }
 
     constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
-
+        init()
     }
 
     constructor(context: Context, attrs: AttributeSet, defStyleAttr: Int) : super(
@@ -20,10 +26,25 @@ class CustomEditText : AppCompatEditText {
         attrs,
         defStyleAttr
     ) {
+        init()
+    }
 
+    private fun init() {
+        imgClearButton =
+            ResourcesCompat.getDrawable(resources, R.drawable.ic_close_black_24dp, null) as Drawable
+    }
+
+    private fun hideClearButton() {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, null, null)
+    }
+
+    private fun showClearButton() {
+        setCompoundDrawablesRelativeWithIntrinsicBounds(null, null, imgClearButton, null)
     }
 
     override fun onDraw(canvas: Canvas?) {
         super.onDraw(canvas)
+        hint = "Masukkan nama Anda"
+        textAlignment = View.TEXT_ALIGNMENT_VIEW_START
     }
 }
