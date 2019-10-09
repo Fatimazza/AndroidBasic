6b0df97abf3e5739593027d567d5ec9988f331d2
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-09 19:41:47 +0700

    Custom View - Add Action for Custom Views in Activity

--- app/src/main/java/io/github/fatimazza/androidbasic/CustomViewActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/CustomViewActivity.kt
@@ -1,12 +1,51 @@
 package io.github.fatimazza.androidbasic
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.TextWatcher
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_custom_view.*
 
 class CustomViewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_custom_view)
+
+        setMyButtonEnable()
+        setTextChangeListener()
+        setButtonClickListener()
+    }
+
+    private fun setMyButtonEnable() {
+        val text = et_edit_custom.text
+        btn_submit_custom.isEnabled = text != null && text.toString().isNotEmpty()
+    }
+
+    private fun setTextChangeListener() {
+        et_edit_custom.addTextChangedListener(object : TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+
+            }
+
+            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                setMyButtonEnable()
+            }
+        })
+    }
+
+    private fun setButtonClickListener() {
+        btn_submit_custom.setOnClickListener {
+            Toast.makeText(
+                this,
+                et_edit_custom.text,
+                Toast.LENGTH_LONG
+            ).show()
+        }
     }
 }
