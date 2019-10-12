daa212827719863154947c3f13950b05c553632d
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-12 21:30:19 +0700

    Localization - Add Menu Item to change Language from Settings

--- app/src/main/java/io/github/fatimazza/androidbasic/LocalizationActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/LocalizationActivity.kt
@@ -1,12 +1,29 @@
 package io.github.fatimazza.androidbasic
 
-import android.support.v7.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.provider.Settings
+import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 
 class LocalizationActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_localization)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_localization, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_language) {
+            val intent = Intent(Settings.ACTION_LOCALE_SETTINGS)
+            startActivity(intent)
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
