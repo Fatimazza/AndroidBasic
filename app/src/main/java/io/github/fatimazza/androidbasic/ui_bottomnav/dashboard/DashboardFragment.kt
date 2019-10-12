fc3360c4639e70ea707d70caae7935bcb0a08b7d
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-12 20:43:57 +0700

    Bottom Navigation - Add Bottom Navigation default Activity

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/ui_bottomnav/dashboard/DashboardFragment.kt
@@ -0,0 +1,31 @@
+package io.github.fatimazza.androidbasic.ui_bottomnav.dashboard
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import io.github.fatimazza.androidbasic.R
+
+class DashboardFragment : Fragment() {
+
+    private lateinit var dashboardViewModel: DashboardViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        dashboardViewModel =
+            ViewModelProviders.of(this).get(DashboardViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_dashboard, container, false)
+        val textView: TextView = root.findViewById(R.id.text_dashboard)
+        dashboardViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
---
+++ app/src/main/java/io/github/fatimazza/androidbasic/ui_bottomnav/notifications/NotificationsFragment.kt
@@ -0,0 +1,31 @@
+package io.github.fatimazza.androidbasic.ui_bottomnav.notifications
+
+import android.arch.lifecycle.Observer
+import android.arch.lifecycle.ViewModelProviders
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import io.github.fatimazza.androidbasic.R
+
+class NotificationsFragment : Fragment() {
+
+    private lateinit var notificationsViewModel: NotificationsViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        notificationsViewModel =
+            ViewModelProviders.of(this).get(NotificationsViewModel::class.java)
+        val root = inflater.inflate(R.layout.fragment_notifications, container, false)
+        val textView: TextView = root.findViewById(R.id.text_notifications)
+        notificationsViewModel.text.observe(this, Observer {
+            textView.text = it
+        })
+        return root
+    }
+}
