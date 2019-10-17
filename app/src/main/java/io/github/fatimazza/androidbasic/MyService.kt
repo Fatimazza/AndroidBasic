25e2e803587405e8d27877eb317b38d6d3733e6f
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-17 19:31:44 +0700

    Service - Implement function in Service class

--- app/src/main/java/io/github/fatimazza/androidbasic/MyService.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/MyService.kt
@@ -3,10 +3,24 @@ package io.github.fatimazza.androidbasic
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
+import android.util.Log
 
 class MyService : Service() {
 
+    companion object {
+        //internal means that it'll be available in the SAME MODULE only
+        internal val TAG = MyService::class.java.simpleName
+    }
+
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        throw UnsupportedOperationException("Not yet implemented")
+    }
+
+    //onStartCommand() used to start the Started Service
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d(TAG, "on Start Command: ")
+        // START_STICKY tells the system to recreate the service after it has enough memory
+        // and call onStartCommand() again with a null intent
+        return START_STICKY
     }
 }
