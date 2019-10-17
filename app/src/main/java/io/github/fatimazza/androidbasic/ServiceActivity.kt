6cd559945a2a4640faee732187aebfffc61f3d84
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-17 18:59:58 +0700

    Service - Add Click Listener for Button to start and stop Service

--- app/src/main/java/io/github/fatimazza/androidbasic/ServiceActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/ServiceActivity.kt
@@ -1,12 +1,36 @@
 package io.github.fatimazza.androidbasic
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_service.*
 
-class ServiceActivity : AppCompatActivity() {
+class ServiceActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_service)
+
+        btn_start_service.setOnClickListener(this)
+        btn_start_intent_service.setOnClickListener(this)
+        btn_start_bound_service.setOnClickListener(this)
+        btn_stop_bound_service.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.btn_start_service -> {
+
+            }
+            R.id.btn_start_intent_service -> {
+
+            }
+            R.id.btn_start_bound_service -> {
+
+            }
+            R.id.btn_stop_bound_service -> {
+                
+            }
+        }
     }
 }
