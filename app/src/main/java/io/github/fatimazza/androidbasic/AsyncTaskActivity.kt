014382be6f01f01b100089ac6c7c92fedff39166
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-17 16:00:14 +0700

    Async Task - Add DemoAsync class to manage data asynchronously

--- app/src/main/java/io/github/fatimazza/androidbasic/AsyncTaskActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/AsyncTaskActivity.kt
@@ -2,6 +2,7 @@ package io.github.fatimazza.androidbasic
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import java.lang.ref.WeakReference
 
 class AsyncTaskActivity : AppCompatActivity(), MyAsyncCallback {
 
@@ -16,6 +17,15 @@ class AsyncTaskActivity : AppCompatActivity(), MyAsyncCallback {
 
     override fun onPostExecute(text: String) {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private class DemoAsync(val listener: MyAsyncCallback) {
+        companion object {
+            private val LOG_ASYNC = "Demo Async"
+        }
+
+        //using WeakReference to avoid Memory Leak in AsyncTask
+        private val myListener: WeakReference<MyAsyncCallback> = WeakReference(listener)
     }
 }
 
