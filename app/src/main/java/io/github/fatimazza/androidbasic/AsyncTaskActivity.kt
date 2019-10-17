d4f168f3baf8a4972d8897487032887fa487e9e3
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-17 16:33:49 +0700

    Coroutine - Refactor Background Thread process from using Async Task to Coroutines

--- app/src/main/java/io/github/fatimazza/androidbasic/AsyncTaskActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/AsyncTaskActivity.kt
@@ -1,13 +1,12 @@
 package io.github.fatimazza.androidbasic
 
-import android.os.AsyncTask
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import kotlinx.android.synthetic.main.activity_async_task.*
-import java.lang.ref.WeakReference
+import kotlinx.coroutines.*
 
-class AsyncTaskActivity : AppCompatActivity(), MyAsyncCallback {
+class AsyncTaskActivity : AppCompatActivity() {
 
     companion object {
         private val LOG_ASYNC = "Demo Async Main"
@@ -18,64 +17,29 @@ class AsyncTaskActivity : AppCompatActivity(), MyAsyncCallback {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_async_task)
 
-        val demoAsync = DemoAsync(this)
-        demoAsync.execute(INPUT_STRING)
-    }
-
-    override fun onPreExecute() {
         Log.d(LOG_ASYNC, "status: onPreExecute - Main")
         tv_async_status.text = resources.getString(R.string.async_status_pre)
         tv_async_desc.text = INPUT_STRING
-    }
 
-    override fun onPostExecute(result: String) {
-        Log.d(LOG_ASYNC, "status: onPostExecute - Main")
-        tv_async_status.text = resources.getString(R.string.async_status_post)
-        tv_async_desc.text = result
-    }
-
-    private class DemoAsync(val listener: MyAsyncCallback) : AsyncTask<String, Void, String>() {
-
-        companion object {
-            private val LOG_ASYNC = "Demo Async"
-        }
-
-        //using WeakReference to avoid Memory Leak in AsyncTask
-        private val myListener: WeakReference<MyAsyncCallback> = WeakReference(listener)
-
-        override fun onPreExecute() {
-            super.onPreExecute()
-            Log.d(LOG_ASYNC, "status: onPreExecute")
-
-            val myListener = myListener.get()
-            myListener?.onPreExecute()
-        }
-
-        override fun doInBackground(vararg params: String?): String {
+        GlobalScope.launch(Dispatchers.IO) {
+            //background thread
             Log.d(LOG_ASYNC, "status: doInBackground")
-
+            val input = INPUT_STRING
             var output: String? = null
             try {
-                val input = params[0]
                 output = "$input ... Done! Selamat Belajar!"
-                Thread.sleep(2000)
+                delay(2000)
+
+                //move to Main Thread to update UI
+                withContext(Dispatchers.Main) {
+                    Log.d(LOG_ASYNC, "status: onPostExecute - Main")
+                    tv_async_status.text = resources.getString(R.string.async_status_post)
+                    tv_async_desc.text = output
+                }
             } catch (e: Exception) {
                 Log.d(LOG_ASYNC, e.message)
             }
-            return output.toString()
-        }
-
-        override fun onPostExecute(result: String) {
-            super.onPostExecute(result)
-            Log.d(LOG_ASYNC, "status: onPostExecute")
-
-            val myListener = myListener.get()
-            myListener?.onPostExecute(result)
         }
     }
 }
 
-internal interface MyAsyncCallback {
-    fun onPreExecute()
-    fun onPostExecute(text: String)
-}
