a97319407c5d5921d2f305489c651d0d0739d609
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-17 14:58:18 +0700

    Async Task - Add Async Interface, Implement it in Activity

--- app/src/main/java/io/github/fatimazza/androidbasic/AsyncTaskActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/AsyncTaskActivity.kt
@@ -1,12 +1,25 @@
 package io.github.fatimazza.androidbasic
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 
-class AsyncTaskActivity : AppCompatActivity() {
+class AsyncTaskActivity : AppCompatActivity(), MyAsyncCallback {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_async_task)
     }
+
+    override fun onPreExecute() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onPostExecute(text: String) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+}
+
+internal interface MyAsyncCallback {
+    fun onPreExecute()
+    fun onPostExecute(text: String)
 }
