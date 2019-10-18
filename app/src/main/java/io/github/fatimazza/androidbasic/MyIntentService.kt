79cd389e6a7be1d909551a479a39d558379232c3
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-18 09:58:32 +0700

    Intent Service - Remove generated codes

--- app/src/main/java/io/github/fatimazza/androidbasic/MyIntentService.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/MyIntentService.kt
@@ -4,50 +4,9 @@ import android.app.IntentService
 import android.content.Intent
 
 
-// TODO: Rename actions, choose action names that describe tasks that this
-// IntentService can perform, e.g. ACTION_FETCH_NEW_ITEMS
-const val ACTION_FOO = "io.github.fatimazza.androidbasic.action.FOO"
-const val ACTION_BAZ = "io.github.fatimazza.androidbasic.action.BAZ"
-
-// TODO: Rename parameters
-const val EXTRA_PARAM1 = "io.github.fatimazza.androidbasic.extra.PARAM1"
-const val EXTRA_PARAM2 = "io.github.fatimazza.androidbasic.extra.PARAM2"
-
-/**
- * An [IntentService] subclass for handling asynchronous task requests in
- * a service on a separate handler thread.
- * TODO: Customize class - update intent actions and extra parameters.
- */
 class MyIntentService : IntentService("MyIntentService") {
 
     override fun onHandleIntent(intent: Intent?) {
-        when (intent?.action) {
-            ACTION_FOO -> {
-                val param1 = intent.getStringExtra(EXTRA_PARAM1)
-                val param2 = intent.getStringExtra(EXTRA_PARAM2)
-                handleActionFoo(param1, param2)
-            }
-            ACTION_BAZ -> {
-                val param1 = intent.getStringExtra(EXTRA_PARAM1)
-                val param2 = intent.getStringExtra(EXTRA_PARAM2)
-                handleActionBaz(param1, param2)
-            }
-        }
-    }
 
-    /**
-     * Handle action Foo in the provided background thread with the provided
-     * parameters.
-     */
-    private fun handleActionFoo(param1: String, param2: String) {
-        TODO("Handle action Foo")
-    }
-
-    /**
-     * Handle action Baz in the provided background thread with the provided
-     * parameters.
-     */
-    private fun handleActionBaz(param1: String, param2: String) {
-        TODO("Handle action Baz")
     }
 }
