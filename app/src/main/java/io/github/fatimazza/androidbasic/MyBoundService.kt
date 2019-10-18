0301a50c73389ea7285ff3249e0582a6f5333d2a
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-18 14:33:12 +0700

    Bound Service - Add Countdown Timer

--- app/src/main/java/io/github/fatimazza/androidbasic/MyBoundService.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/MyBoundService.kt
@@ -3,7 +3,9 @@ package io.github.fatimazza.androidbasic
 import android.app.Service
 import android.content.Intent
 import android.os.Binder
+import android.os.CountDownTimer
 import android.os.IBinder
+import android.util.Log
 
 class MyBoundService : Service() {
 
@@ -12,6 +14,20 @@ class MyBoundService : Service() {
     }
 
     private var myBinder = MyBinder()
+    private val startTime = System.currentTimeMillis()
+
+    private var timer: CountDownTimer = object : CountDownTimer(100000, 1000) {
+        //countdown run till 100.000ms or 100 second, interval 1000ms or 1 second
+        //used to count bound process of Bound Services to Activity
+        override fun onTick(l: Long) {
+            val elapsedTime = System.currentTimeMillis() - startTime
+            Log.d(TAG, "onTick: $elapsedTime")
+        }
+
+        override fun onFinish() {
+
+        }
+    }
 
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
