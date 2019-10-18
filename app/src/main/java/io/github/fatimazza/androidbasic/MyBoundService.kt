bb8ff1c9eed542a2dc8216ad23d162b18f68913e
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-18 14:23:27 +0700

    Bound Service - Add MyBinder Class to bind the Service class

--- app/src/main/java/io/github/fatimazza/androidbasic/MyBoundService.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/MyBoundService.kt
@@ -2,11 +2,23 @@ package io.github.fatimazza.androidbasic
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 
 class MyBoundService : Service() {
 
+    companion object {
+        private val TAG = MyBoundService::class.java.simpleName
+    }
+
+    private var myBinder = MyBinder()
+
     override fun onBind(intent: Intent): IBinder {
         TODO("Return the communication channel to the service.")
     }
+
+    internal inner class MyBinder : Binder() {
+        val getService: MyBoundService = this@MyBoundService
+    }
+
 }
