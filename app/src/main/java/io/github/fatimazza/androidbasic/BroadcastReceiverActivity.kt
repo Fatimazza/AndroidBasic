99273c9b9a2ea4dd571bada3e76b763a1564a0dc
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-19 15:03:18 +0700

    Broadcast Receiver SMS - Add Layout and Click Listener

--- app/src/main/java/io/github/fatimazza/androidbasic/BroadcastReceiverActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/BroadcastReceiverActivity.kt
@@ -1,12 +1,24 @@
 package io.github.fatimazza.androidbasic
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import kotlinx.android.synthetic.main.activity_broadcast_receiver.*
 
-class BroadcastReceiverActivity : AppCompatActivity() {
+class BroadcastReceiverActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_broadcast_receiver)
+
+        btn_br_sms_permission.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        when {
+            view.id == R.id.btn_br_sms_permission -> {
+                
+            }
+        }
     }
 }
