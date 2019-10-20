8ad6644bab1e92c962f067f01db438d3cf84e26b
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-20 13:48:38 +0700

    View Model - Show List Weather in Activity

--- app/src/main/java/io/github/fatimazza/androidbasic/WeatherActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/WeatherActivity.kt
@@ -2,11 +2,26 @@ package io.github.fatimazza.androidbasic
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import io.github.fatimazza.androidbasic.adapter.WeatherAdapter
+import kotlinx.android.synthetic.main.activity_weather.*
 
 class WeatherActivity : AppCompatActivity() {
+
+    private lateinit var adapter: WeatherAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
+
+        showListWeather()
+    }
+
+    private fun showListWeather() {
+        adapter = WeatherAdapter()
+        adapter.notifyDataSetChanged()
+
+        rvWeather.layoutManager = LinearLayoutManager(this)
+        rvWeather.adapter = adapter
     }
 }
