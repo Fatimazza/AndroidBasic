fdaccad0a5aa38f97d0ec83288dff141ecd226da
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-20 14:16:24 +0700

    View Model - Init Weather View Model

--- app/src/main/java/io/github/fatimazza/androidbasic/WeatherActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/WeatherActivity.kt
@@ -3,19 +3,23 @@ package io.github.fatimazza.androidbasic
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import io.github.fatimazza.androidbasic.adapter.WeatherAdapter
+import io.github.fatimazza.androidbasic.viewmodel.WeatherViewModel
 import kotlinx.android.synthetic.main.activity_weather.*
 
 class WeatherActivity : AppCompatActivity() {
 
     private lateinit var adapter: WeatherAdapter
+    private lateinit var weatherViewModel: WeatherViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_weather)
 
         showListWeather()
+        initViewModel()
     }
 
     private fun showListWeather() {
@@ -24,6 +28,11 @@ class WeatherActivity : AppCompatActivity() {
 
         rvWeather.layoutManager = LinearLayoutManager(this)
         rvWeather.adapter = adapter
+    }
+
+    private fun initViewModel() {
+        weatherViewModel = ViewModelProvider(this, ViewModelProvider.NewInstanceFactory())
+            .get(WeatherViewModel::class.java)
     }
 
     private fun showLoading(state: Boolean) {
