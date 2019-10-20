55553813bd11386776b85c950ac4c8218f19a3de
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-20 13:32:10 +0700

    View Model - Implement functions in Weather Adapter

--- app/src/main/java/io/github/fatimazza/androidbasic/adapter/WeatherAdapter.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/adapter/WeatherAdapter.kt
@@ -1,24 +1,42 @@
 package io.github.fatimazza.androidbasic.adapter
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import io.github.fatimazza.androidbasic.R
+import io.github.fatimazza.androidbasic.model.Weather
+import kotlinx.android.synthetic.main.item_weather.view.*
 
 class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() {
 
+    private val weatherData = ArrayList<Weather>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val mView =
+            LayoutInflater.from(parent.context).inflate(R.layout.item_weather, parent, false)
+        return WeatherViewHolder(mView)
     }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemCount(): Int = weatherData.size
 
     override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        holder.bind(weatherData[position])
+    }
+
+    fun setData(weatherItems: ArrayList<Weather>) {
+        weatherData.clear()
+        weatherData.addAll(weatherItems)
+        notifyDataSetChanged()
     }
 
     inner class WeatherViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        fun bind(weatherItem: Weather) {
+            with(itemView) {
+                tvWeatherCity.text = weatherItem.name
+                tvWeatherTemp.text = weatherItem.temperature
+                tvWeatherDesc.text = weatherItem.description
+            }
+        }
     }
 }
