f33415a5c7dffd62de0763c9178e546c482998a3
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-20 09:29:10 +0700

    Broadcast Receiver Download - Add initial Intent Service for Download

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/service/DownloadService.kt
@@ -0,0 +1,12 @@
+package io.github.fatimazza.androidbasic.service
+
+import android.app.IntentService
+import android.content.Intent
+
+
+class DownloadService : IntentService("DownloadService") {
+
+    override fun onHandleIntent(intent: Intent?) {
+
+    }
+}
