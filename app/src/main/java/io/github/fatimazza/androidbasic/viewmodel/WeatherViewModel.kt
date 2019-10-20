5b8a7909f64228e5b7dd34ecfd426c0b01f4cc08
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-20 13:43:19 +0700

    View Model - Configure Build Config for API KEY

--- app/src/main/java/io/github/fatimazza/androidbasic/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/viewmodel/WeatherViewModel.kt
@@ -3,9 +3,14 @@ package io.github.fatimazza.androidbasic.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import io.github.fatimazza.androidbasic.BuildConfig
 import io.github.fatimazza.androidbasic.model.Weather
 
 class WeatherViewModel : ViewModel() {
+
+    companion object {
+        private const val API_KEY = BuildConfig.API_KEY
+    }
 
     private val listWeathers = MutableLiveData<ArrayList<Weather>>()
 
