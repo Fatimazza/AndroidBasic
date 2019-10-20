baf343973a47b97936e0e65a9469c59a0a89d1cf
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-20 13:25:23 +0700

    View Model - Add Initial Adapter, Add Weather Model

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/adapter/WeatherAdapter.kt
@@ -0,0 +1,24 @@
+package io.github.fatimazza.androidbasic.adapter
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class WeatherAdapter : RecyclerView.Adapter<WeatherAdapter.WeatherViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WeatherViewHolder {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onBindViewHolder(holder: WeatherViewHolder, position: Int) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    inner class WeatherViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+}
---
+++ app/src/main/java/io/github/fatimazza/androidbasic/model/Weather.kt
@@ -0,0 +1,9 @@
+package io.github.fatimazza.androidbasic.model
+
+class Weather {
+    var id: Int = 0
+    var name: String? = null
+    var currentWeather: String? = null
+    var description: String? = null
+    var temperature: String? = null
+}
