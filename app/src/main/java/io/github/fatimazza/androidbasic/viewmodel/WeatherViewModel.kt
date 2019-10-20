17cfd6c2629aebcd2abd1ebac025eaa8d2c22321
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-20 13:57:39 +0700

    View Model - Request API using LoopJ Async Http Client in View Model

--- app/src/main/java/io/github/fatimazza/androidbasic/viewmodel/WeatherViewModel.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/viewmodel/WeatherViewModel.kt
@@ -1,8 +1,12 @@
 package io.github.fatimazza.androidbasic.viewmodel
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.loopj.android.http.AsyncHttpClient
+import com.loopj.android.http.AsyncHttpResponseHandler
+import cz.msebera.android.httpclient.Header
 import io.github.fatimazza.androidbasic.BuildConfig
 import io.github.fatimazza.androidbasic.model.Weather
 
@@ -15,7 +19,30 @@ class WeatherViewModel : ViewModel() {
     private val listWeathers = MutableLiveData<ArrayList<Weather>>()
 
     internal fun setWeather(city: String) {
+        val client = AsyncHttpClient()
+        val listItems = ArrayList<Weather>()
+        val url =
+            "https://api.openweathermap.org/data/2.5/group?id=$city&units=metric&appid=$API_KEY"
 
+        //request API
+        client.get(url, object : AsyncHttpResponseHandler() {
+            override fun onSuccess(
+                statusCode: Int,
+                headers: Array<out Header>,
+                responseBody: ByteArray
+            ) {
+                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+            }
+
+            override fun onFailure(
+                statusCode: Int,
+                headers: Array<out Header>,
+                responseBody: ByteArray,
+                error: Throwable
+            ) {
+                Log.d("onFailure", error.message.toString())
+            }
+        })
     }
 
     internal fun getWeather(): LiveData<ArrayList<Weather>> {
