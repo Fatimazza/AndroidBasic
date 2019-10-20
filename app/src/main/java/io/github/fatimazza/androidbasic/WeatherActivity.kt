70ae03d1a14119b3b1d52ac5be72b9a4fcbf0ed6
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-20 11:46:14 +0700

    View Model - Add Weather Activity for View Model demo

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/WeatherActivity.kt
@@ -0,0 +1,12 @@
+package io.github.fatimazza.androidbasic
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class WeatherActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_weather)
+    }
+}
