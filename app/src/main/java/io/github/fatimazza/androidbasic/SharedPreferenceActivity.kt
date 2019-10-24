137f00c905e92399a5699bb729b7fb5a5cd62185
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-24 11:03:48 +0700

    Shared Preference - Add new Activity for Shared Preference demo

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/SharedPreferenceActivity.kt
@@ -0,0 +1,12 @@
+package io.github.fatimazza.androidbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SharedPreferenceActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shared_preference)
+    }
+}
