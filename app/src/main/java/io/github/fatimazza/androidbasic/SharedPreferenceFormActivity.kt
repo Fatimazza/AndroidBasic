6ddf184ffef91b4902fc526d1cf69c2fa0fbbaf1
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-24 11:27:25 +0700

    Shared Preference - Add new Activity for Shared Preference Form

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/SharedPreferenceFormActivity.kt
@@ -0,0 +1,12 @@
+package io.github.fatimazza.androidbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SharedPreferenceFormActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shared_preference_form)
+    }
+}
