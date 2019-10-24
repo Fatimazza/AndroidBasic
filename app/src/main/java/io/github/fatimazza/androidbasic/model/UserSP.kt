2939042055bc802e24ad75c061133e9e72281304
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-24 11:16:31 +0700

    Shared Preference - Add needed Model

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/model/UserSP.kt
@@ -0,0 +1,13 @@
+package io.github.fatimazza.androidbasic.model
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
+data class UserSP(
+    var name: String? = null,
+    var email: String? = null,
+    var age: Int = 0,
+    var handphone: String? = null,
+    var hasReadingHobby: Boolean = false
+) : Parcelable
