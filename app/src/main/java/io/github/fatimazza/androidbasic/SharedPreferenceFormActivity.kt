0b5e2613516b3ba8d97dce892041aa0eb6a59e1d
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-24 11:43:53 +0700

    Shared Preference - Prepare Activity of Shared Preference Form

--- app/src/main/java/io/github/fatimazza/androidbasic/SharedPreferenceFormActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/SharedPreferenceFormActivity.kt
@@ -1,12 +1,33 @@
 package io.github.fatimazza.androidbasic
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import io.github.fatimazza.androidbasic.model.SPUserModel
+import kotlinx.android.synthetic.main.activity_shared_preference_form.*
 
-class SharedPreferenceFormActivity : AppCompatActivity() {
+class SharedPreferenceFormActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        const val EXTRA_TYPE_FORM = "extra_type_form"
+        const val EXTRA_RESULT = "extra_result"
+        const val RESULT_CODE = 101
+        const val TYPE_ADD = 1
+        const val TYPE_EDIT = 2
+    }
+
+    private lateinit var userModel: SPUserModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_shared_preference_form)
+
+        btnPrefSave.setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.btnPrefSave) {
+
+        }
     }
 }
