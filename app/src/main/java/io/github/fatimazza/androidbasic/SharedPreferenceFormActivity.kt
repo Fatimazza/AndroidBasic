0b936c1e347584693df9f8fd7916e90d1418b967
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-10-24 12:02:17 +0700

    Shared Preference - Add Logic for all Fields in Shared Preference Form

--- app/src/main/java/io/github/fatimazza/androidbasic/SharedPreferenceFormActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/SharedPreferenceFormActivity.kt
@@ -3,6 +3,7 @@ package io.github.fatimazza.androidbasic
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.isDigitsOnly
 import io.github.fatimazza.androidbasic.model.SPUserModel
 import kotlinx.android.synthetic.main.activity_shared_preference_form.*
 
@@ -27,7 +28,47 @@ class SharedPreferenceFormActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(view: View) {
         if (view.id == R.id.btnPrefSave) {
+            val name = etPrefName.text.toString().trim()
+            val email = etPrefEmail.text.toString().trim()
+            val age = etPrefAge.text.toString().trim()
+            val handphone = etPrefHp.text.toString().trim()
+            val hasReadingHobby = rgPrefHobby.checkedRadioButtonId == R.id.rbPrefReading
+
+            if (name.isEmpty()) {
+                etPrefName.error = resources.getString(R.string.sp_field_required)
+                return
+            }
+
+            if (email.isEmpty()) {
+                etPrefEmail.error = resources.getString(R.string.sp_field_required)
+                return
+            }
+
+            if (!isValidEmail(email)) {
+                etPrefEmail.error = resources.getString(R.string.sp_field_email_not_valid)
+                return
+            }
+
+            if (age.isEmpty()) {
+                etPrefAge.error = resources.getString(R.string.sp_field_required)
+                return
+            }
+
+            if (handphone.isEmpty()) {
+                etPrefHp.error = resources.getString(R.string.sp_field_required)
+                return
+            }
+
+            if (!handphone.isDigitsOnly()) {
+                etPrefHp.error = resources.getString(R.string.sp_field_digit_only)
+                return
+            }
+            
 
         }
+    }
+
+    private fun isValidEmail(email: CharSequence): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()
     }
 }
