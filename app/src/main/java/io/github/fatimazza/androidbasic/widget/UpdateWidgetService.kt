fc52d79e9125f3222ffa95a8e4aaa8d13901a368
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-11-07 19:41:11 +0700

    Widget - Make JobService has same logic as Widget updateAppWidget

--- app/src/main/java/io/github/fatimazza/androidbasic/widget/UpdateWidgetService.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/widget/UpdateWidgetService.kt
@@ -2,16 +2,31 @@ package io.github.fatimazza.androidbasic.widget
 
 import android.app.job.JobParameters
 import android.app.job.JobService
+import android.appwidget.AppWidgetManager
+import android.content.ComponentName
+import android.widget.RemoteViews
+import io.github.fatimazza.androidbasic.R
 
 //JobScheduler only works in API >21
 class UpdateWidgetService : JobService() {
 
     override fun onStartJob(jobParameters: JobParameters): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        //get random number
+        //logic same as updateAppWidget() inside RandomNumber Widget
+
+        val manager = AppWidgetManager.getInstance(this)
+        val randomNumberWidget = ComponentName(this, RandomNumberWidget::class.java)
+
+        val lastUpdate = "Random: " + NumberGenerator.generate(100)
+        val views = RemoteViews(packageName, R.layout.random_number_widget)
+        views.setTextViewText(R.id.appwidget_text, lastUpdate)
+
+        //update from Service will update all Widgets
+        manager.updateAppWidget(randomNumberWidget, views)
+        jobFinished(jobParameters, false)
+        return true
     }
 
-    override fun onStopJob(jobParameters: JobParameters): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun onStopJob(jobParameters: JobParameters): Boolean = false
 
 }
