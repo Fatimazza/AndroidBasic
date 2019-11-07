aebb35eecbb28022d5fea214cfb3c098d9a5d742
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-11-07 20:21:25 +0700

    Widget - Add function to start and stop JobScheduler

--- app/src/main/java/io/github/fatimazza/androidbasic/WidgetActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/WidgetActivity.kt
@@ -1,8 +1,15 @@
 package io.github.fatimazza.androidbasic
 
+import android.app.job.JobInfo
+import android.app.job.JobScheduler
+import android.content.ComponentName
+import android.content.Context
+import android.os.Build
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import io.github.fatimazza.androidbasic.widget.UpdateWidgetService
 
 class WidgetActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -28,10 +35,30 @@ class WidgetActivity : AppCompatActivity(), View.OnClickListener {
     }
 
     private fun startJob() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val serviceComponent = ComponentName(this, UpdateWidgetService::class.java)
+
+        val builder = JobInfo.Builder(JOB_ID, serviceComponent)
+        builder.setRequiredNetworkType(JobInfo.NETWORK_TYPE_NONE)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            //in Nougat, Job schedule is limited to 15 min
+            builder.setPeriodic(90000) //90000L = 15min
+        } else {
+            builder.setPeriodic(SCHEDULE_OF_PERIOD) //86000L = 3min
+        }
+
+        // updatePeriodMillis in appwidget-provider should set to 0
+        // to avoid conflict between:
+        // scheduled update from Widget (0) and scheduled update from JobScheduler
+
+        val jobScheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        jobScheduler.schedule(builder.build())
+
+        Toast.makeText(this, getString(R.string.widget_job_started), Toast.LENGTH_SHORT).show()
     }
 
     private fun cancelJob() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val jobScheduler = getSystemService(Context.JOB_SCHEDULER_SERVICE) as JobScheduler
+        jobScheduler.cancel(JOB_ID)
+        Toast.makeText(this, getString(R.string.widget_job_cancelled), Toast.LENGTH_SHORT).show()
     }
 }
