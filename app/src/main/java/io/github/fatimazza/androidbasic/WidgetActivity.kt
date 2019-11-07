7a064a766a97d237c495edbd2bd269459069247c
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-11-07 19:59:40 +0700

    Widget - Add Button and Click Listener to start and stop Job

--- app/src/main/java/io/github/fatimazza/androidbasic/WidgetActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/WidgetActivity.kt
@@ -1,12 +1,37 @@
 package io.github.fatimazza.androidbasic
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
 
-class WidgetActivity : AppCompatActivity() {
+class WidgetActivity : AppCompatActivity(), View.OnClickListener {
+
+    companion object {
+        private const val JOB_ID = 100
+        private const val SCHEDULE_OF_PERIOD = 86000L
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_widget)
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.btn_jobscheduler_widget_start -> {
+                startJob()
+            }
+            R.id.btn_jobscheduler_widget_stop -> {
+                cancelJob()
+            }
+        }
+    }
+
+    private fun startJob() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    private fun cancelJob() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
     }
 }
