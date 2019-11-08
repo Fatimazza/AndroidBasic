14064ec343337f8627653500654ec0d4b397df3c
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-11-08 13:25:11 +0700

    Stack Widget - Add Remote Views Factory Class

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/widget/StackRemoteViewFactory.kt
@@ -0,0 +1,44 @@
+package io.github.fatimazza.androidbasic.widget
+
+import android.widget.RemoteViews
+import android.widget.RemoteViewsService
+
+internal class StackRemoteViewFactory : RemoteViewsService.RemoteViewsFactory {
+
+    override fun onCreate() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun hasStableIds(): Boolean {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+    
+    override fun getLoadingView(): RemoteViews {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getItemId(i: Int): Long {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getViewTypeCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getViewAt(position: Int): RemoteViews {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun getCount(): Int {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onDataSetChanged() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onDestroy() {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
