cfffc88fc4ced94e90f72795e17a0374307f4715
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-11-08 13:34:03 +0700

    Stack Widget - Implement functions in Remote Views Factory Class

--- app/src/main/java/io/github/fatimazza/androidbasic/widget/StackRemoteViewFactory.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/widget/StackRemoteViewFactory.kt
@@ -1,44 +1,65 @@
 package io.github.fatimazza.androidbasic.widget
 
+import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.widget.RemoteViews
 import android.widget.RemoteViewsService
+import androidx.core.os.bundleOf
+import io.github.fatimazza.androidbasic.R
 
-internal class StackRemoteViewFactory : RemoteViewsService.RemoteViewsFactory {
+internal class StackRemoteViewFactory(private val mContext: Context) :
+    RemoteViewsService.RemoteViewsFactory {
+
+    private val mWidgetItems = ArrayList<Bitmap>()
 
     override fun onCreate() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
-    override fun hasStableIds(): Boolean {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-    
-    override fun getLoadingView(): RemoteViews {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun hasStableIds(): Boolean = false
 
-    override fun getItemId(i: Int): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getLoadingView(): RemoteViews? = null
 
-    override fun getViewTypeCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getItemId(i: Int): Long = 0
+
+    override fun getViewTypeCount(): Int = 1
 
     override fun getViewAt(position: Int): RemoteViews {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        val rv = RemoteViews(mContext.packageName, R.layout.item_image_widget)
+        rv.setImageViewBitmap(R.id.ivImageWidget, mWidgetItems[position])
+
+        val extras = bundleOf(ImageStackWidget.EXTRA_ITEM to position)
+        val fillInIntent = Intent()
+        fillInIntent.putExtras(extras)
+
+        rv.setOnClickFillInIntent(R.id.imageView, fillInIntent)
+        return rv
     }
 
-    override fun getCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+    override fun getCount(): Int = mWidgetItems.size
 
     override fun onDataSetChanged() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        mWidgetItems.add(
+            BitmapFactory.decodeResource(mContext.resources, R.drawable.starwars_darth_vader)
+        )
+        mWidgetItems.add(
+            BitmapFactory.decodeResource(mContext.resources, R.drawable.starwars_logo)
+        )
+        mWidgetItems.add(
+            BitmapFactory.decodeResource(mContext.resources, R.drawable.starwars_storm_trooper)
+        )
+        mWidgetItems.add(
+            BitmapFactory.decodeResource(mContext.resources, R.drawable.starwars)
+        )
+        mWidgetItems.add(
+            BitmapFactory.decodeResource(mContext.resources, R.drawable.starwars_falcon)
+        )
     }
 
     override fun onDestroy() {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+
     }
 
 }
