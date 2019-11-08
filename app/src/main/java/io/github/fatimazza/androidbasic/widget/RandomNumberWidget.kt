45d7045799a8693318751d23325f537ca0c34dcd
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-11-08 12:06:20 +0700

    Widget with Button - Fix needed function in Widget Class

--- app/src/main/java/io/github/fatimazza/androidbasic/widget/RandomNumberWidget.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/widget/RandomNumberWidget.kt
@@ -32,32 +32,31 @@ class RandomNumberWidget : AppWidgetProvider() {
         // Enter relevant functionality for when the last widget is disabled
     }
 
-    override fun onReceive(context: Context?, intent: Intent?) {
+    override fun onReceive(context: Context, intent: Intent) {
         super.onReceive(context, intent)
     }
 
-    private fun getPendingSelfIntent() {
-        
-    }
-
     companion object {
-
         private const val WIDGET_CLICK = "widgetsclick"
         private const val WIDGET_ID_EXTRA = "widget_id_extra"
+    }
 
-        internal fun updateAppWidget(
-            context: Context, appWidgetManager: AppWidgetManager,
-            appWidgetId: Int
-        ) {
+    fun updateAppWidget(
+        context: Context, appWidgetManager: AppWidgetManager,
+        appWidgetId: Int
+    ) {
 
-            val lastUpdate = "Random: " + NumberGenerator.generate(100)
-            // Construct the RemoteViews object
-            val views = RemoteViews(context.packageName, R.layout.random_number_widget)
-            views.setTextViewText(R.id.appwidget_text, lastUpdate)
-            
-            // Instruct the widget manager to update the widget
-            appWidgetManager.updateAppWidget(appWidgetId, views)
-        }
+        val lastUpdate = "Random: " + NumberGenerator.generate(100)
+        // Construct the RemoteViews object
+        val views = RemoteViews(context.packageName, R.layout.random_number_widget)
+        views.setTextViewText(R.id.appwidget_text, lastUpdate)
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views)
+    }
+
+    private fun getPendingSelfIntent() {
+
     }
 }
 
