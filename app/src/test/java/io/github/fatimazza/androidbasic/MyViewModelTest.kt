63eb6f617be7b038d24995ba5966ad99434b1d9a
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-01 14:57:21 +0700

    VM Unit Testing - Test calculate with Number Format Exception

--- app/src/test/java/io/github/fatimazza/androidbasic/MyViewModelTest.kt
+++ app/src/test/java/io/github/fatimazza/androidbasic/MyViewModelTest.kt
@@ -3,11 +3,17 @@ package io.github.fatimazza.androidbasic
 import io.github.fatimazza.androidbasic.viewmodel.MyViewModel
 import org.junit.Assert.assertEquals
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
+import org.junit.rules.ExpectedException
 
 class MyViewModelTest {
 
     private lateinit var myViewModel: MyViewModel
+
+    @Rule
+    @JvmField // fix rule must be public
+    public var thrown = ExpectedException.none()
 
     @Before
     fun init() {
@@ -21,5 +27,18 @@ class MyViewModelTest {
         val height: String = "4"
         myViewModel.calculate(length, width, height)
         assertEquals(24, myViewModel.result)
+    }
+
+    @Test
+    @Throws(java.lang.NumberFormatException::class)
+    fun doubleInputCalculateTest() {
+        val length: String = "2.5" //java.lang.NumberFormatException: For input string: "2.5"
+        val width: String = "3"
+        val height: String = "4"
+
+        thrown.expect(NumberFormatException::class.java)
+        thrown.expectMessage("For input string: \"2.5\"")
+
+        myViewModel.calculate(length, width, height)
     }
 }
