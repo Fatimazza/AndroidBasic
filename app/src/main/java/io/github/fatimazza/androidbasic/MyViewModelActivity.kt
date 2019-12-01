29811f65980aca13a02842af169a4e855a149a30
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-01 14:24:10 +0700

    ViewModel - Get all Views

--- app/src/main/java/io/github/fatimazza/androidbasic/MyViewModelActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/MyViewModelActivity.kt
@@ -1,11 +1,30 @@
 package io.github.fatimazza.androidbasic
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProviders
 import io.github.fatimazza.androidbasic.viewmodel.MyViewModel
+import kotlinx.android.synthetic.main.activity_myview_model.*
 
 class MyViewModelActivity : AppCompatActivity() {
+
+    private val tvResult: TextView
+        get() = tv_result
+
+    private val btnCalculate: Button
+        get() = btn_calculate
+
+    private val etLength: EditText
+        get() = edt_length
+
+    private val etWidth: EditText
+        get() = edt_width
+
+    private val etHeight: EditText
+        get() = edt_height
 
     private lateinit var myViewModel: MyViewModel
 
