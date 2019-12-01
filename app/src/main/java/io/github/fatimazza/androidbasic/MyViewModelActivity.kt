0c437039fc421b850f33d922f6de10cb23418693
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-01 14:00:11 +0700

    View Model - Add new Activity for demo View Model

---
+++ app/src/main/java/io/github/fatimazza/androidbasic/MyViewModelActivity.kt
@@ -0,0 +1,12 @@
+package io.github.fatimazza.androidbasic
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MyViewModelActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_myview_model)
+    }
+}
