98ddf9ddfb9c63b2f9e2ed7e57925b490e393c20
Fatima Azzahro <azzahro.fatima@gmail.com>
2019-12-01 14:44:18 +0700

    VM Unit Testing - Test calculate Function in View Model

--- app/src/test/java/io/github/fatimazza/androidbasic/MyViewModelTest.kt
+++ app/src/test/java/io/github/fatimazza/androidbasic/MyViewModelTest.kt
@@ -1,11 +1,25 @@
 package io.github.fatimazza.androidbasic
 
+import io.github.fatimazza.androidbasic.viewmodel.MyViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Before
 import org.junit.Test
 
 class MyViewModelTest {
 
+    private lateinit var myViewModel: MyViewModel
+
+    @Before
+    fun init() {
+        myViewModel = MyViewModel()
+    }
+
     @Test
     fun calculate() {
-        
+        val length: String = "2"
+        val width: String = "3"
+        val height: String = "4"
+        myViewModel.calculate(length, width, height)
+        assertEquals(24, myViewModel.result)
     }
 }
