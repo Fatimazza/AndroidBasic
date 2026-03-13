68117b7589a93d36923f02b0278434f89e57b592
Fatima Azzahro <azzahro.fatima@gmail.com>
2026-03-13 11:46:20 +0700

    Intent - Open Intent activity from Home

--- app/src/main/java/io/github/fatimazza/androidbasic/HomeActivity.kt
+++ app/src/main/java/io/github/fatimazza/androidbasic/HomeActivity.kt
@@ -9,18 +9,21 @@ import androidx.appcompat.app.AppCompatActivity
 class HomeActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var btnCalculateVolume: Button
+    private lateinit var btnIntent: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
 
         btnCalculateVolume = findViewById(R.id.btnCalculateVolume)
+        btnIntent = findViewById(R.id.btnIntent)
 
         setClickListener()
     }
 
     private fun setClickListener() {
         btnCalculateVolume.setOnClickListener(this)
+        btnIntent.setOnClickListener(this)
     }
 
     override fun onClick(view: View) {
@@ -28,6 +31,10 @@ class HomeActivity : AppCompatActivity(), View.OnClickListener {
             R.id.btnCalculateVolume -> {
                 val calculationIntent = Intent(this, MainActivity::class.java)
                 startActivity(calculationIntent)
+            }
+            R.id.btnIntent -> {
+                val demoIntent = Intent(this, IntentActivity::class.java)
+                startActivity(demoIntent)
             }
         }
     }
